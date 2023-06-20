cb504e45366acf7b161ce6f7652b6b92bb3414eb
Mike Helder Ribeiro <mikehelder01@hotmail.com>
2023-06-20 15:00:33 -0300

    entidades

---
+++ src/entities/FundoDespesasOcasionais.java
@@ -0,0 +1,60 @@
+package entities;
+
+public class FundoDespesasOcasionais {
+	
+	private int codigo;
+	private String nome;
+	private float valor;
+	private boolean recorrencia;
+	
+	public FundoDespesasOcasionais() {
+	}
+
+	public FundoDespesasOcasionais(int codigo, String nome, float valor, boolean recorrencia) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.valor = valor;
+		this.recorrencia = recorrencia;
+	}
+
+	@Override
+	public String toString() {
+		return "Investimento [codigo=" + codigo + ", nome=" + nome + ", valor=" + valor + ", recorrencia=" + recorrencia
+				+ "]";
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+
+	public boolean isRecorrencia() {
+		return recorrencia;
+	}
+
+	public void setRecorrencia(boolean recorrencia) {
+		this.recorrencia = recorrencia;
+	}
+	
+	
+}
---
+++ src/entities/Rendimento.java
@@ -0,0 +1,71 @@
+package entities;
+
+public class Rendimento {
+
+	private int codigo;
+	private String categoria;
+	private String nome;
+	private float valor;
+	private boolean recorrencia;
+	
+	public Rendimento() {
+		
+	}
+	
+	public Rendimento(int codigo, String categoria, String nome, float valor, boolean recorrencia) {
+		super();
+		this.codigo = codigo;
+		this.categoria = categoria;
+		this.nome = nome;
+		this.valor = valor;
+		this.recorrencia = recorrencia;
+	}
+
+	@Override
+	public String toString() {
+		return "Rendimento [codigo=" + codigo + ", categoria=" + categoria + ", nome=" + nome + ", valor=" + valor
+				+ ", recorrencia=" + recorrencia + "]";
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+
+	public boolean isRecorrencia() {
+		return recorrencia;
+	}
+
+	public void setRecorrencia(boolean recorrencia) {
+		this.recorrencia = recorrencia;
+	}
+	
+	
+}
