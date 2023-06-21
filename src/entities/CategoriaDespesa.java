bd87a74e0bd61e440b4733caf00e8dcae3fa5810
ViniciusNM754 <viniciusnogueiramartins@alunos.utfpr.edu.br>
2023-06-20 21:00:31 -0300

    Revert "Revert "add categoriadespesa""
    This reverts commit 0b7e82bfbd7ea0988dac1d9a45915027f95d3ab1.

---
+++ src/entities/CategoriaDespesa.java
@@ -0,0 +1,31 @@
+package entities;
+
+public class CategoriaDespesa {
+	int codigo;
+	String descricao;
+	
+	public CategoriaDespesa() {
+	}
+	
+	public CategoriaDespesa(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public CategoriaDespesa(int codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
