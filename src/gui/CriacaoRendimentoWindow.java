e5894062e3dd0d12b2e2331dcea710e6e8a6a317
ViniciusNM754 <viniciusnogueiramartins@alunos.utfpr.edu.br>
2023-06-21 19:33:38 -0300

    Update CriacaoRendimentoWindow.java

--- src/gui/CriacaoRendimentoWindow.java
+++ src/gui/CriacaoRendimentoWindow.java
@@ -88,7 +88,6 @@ public class CriacaoRendimentoWindow extends JFrame {
 		
 		try {
 			this.buscarCategorias();
-			this.btnSendActionperformed();
 		} catch (SQLException e) {
 			JOptionPane.showMessageDialog(null,"SQLException", "Error", JOptionPane.ERROR_MESSAGE);
 			e.printStackTrace();
