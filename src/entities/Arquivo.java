bf386cc6d557d0f631e63b8d419b2c5c02240b5e
Mike Helder Ribeiro <mikehelder01@hotmail.com>
2023-06-21 19:44:04 -0300

    to string das categorias

---
+++ src/entities/Arquivo.java
@@ -0,0 +1,14 @@
+package entities;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+
+public class Arquivo {
+
+	
+	
+}
