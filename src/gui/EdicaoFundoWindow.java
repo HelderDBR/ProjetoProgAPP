572ac04d927771a2bca1120c934d88c3a2204b16
ViniciusNM754 <viniciusnogueiramartins@alunos.utfpr.edu.br>
2023-06-21 23:25:55 -0300

    fsgdfsgfdgfd

--- src/gui/EdicaoFundoWindow.java
+++ src/gui/EdicaoFundoWindow.java
@@ -6,9 +6,39 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import entities.FundoDespesasOcasionais;
+import entities.Investimento;
+import service.FundoDespesasOcasionaisService;
+import service.InvestimentoService;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
+
 public class EdicaoFundoWindow extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textName;
+	private JTextField textValue;
+	private JLabel lblName;
+	private JLabel lblValue;
+	private JLabel lblNewLabel_2;
+	private JLabel lblNewLabel_3;
+	private JSpinner spinnerMes;
+	private JSpinner spinnerAno;
+	private FundoDespesasOcasionaisService fundoService;
+	private FundoDespesasOcasionais fundo;
+	private JButton btnSend;
+	private JComboBox comboBox;
+	private JLabel lblNewLabel;
 
 	/**
 	 * Launch the application.
@@ -30,12 +60,114 @@ public class EdicaoFundoWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public EdicaoFundoWindow() {
+		setTitle("Cadastro de Investimento");
+		this.setResizable(false);
+		this.initComponents();
+		this.fundoService = new FundoDespesasOcasionaisService();
+		
+		try {
+			this.buscarInvestimentos();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void buscarInvestimentos() throws SQLException, IOException {
+		List<FundoDespesasOcasionais> fundos = this.fundoService.buscarFundoDespesasOcasionais();
+		
+		for (FundoDespesasOcasionais fundo : fundos) {
+				comboBox.addItem(fundo);
+		}
+		
+	}
+
+	private void initComponents() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 291, 256);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		lblName = new JLabel("Digite o Nome:");
+		lblName.setBounds(10, 62, 96, 13);
+		contentPane.add(lblName);
+		
+		textName = new JTextField();
+		textName.setText("Nome");
+		textName.setBounds(10, 85, 96, 19);
+		contentPane.add(textName);
+		textName.setColumns(10);
+		
+		lblValue = new JLabel("Digite o Valor:");
+		lblValue.setBounds(10, 114, 96, 13);
+		contentPane.add(lblValue);
+		
+		textValue = new JTextField();
+		textValue.setText("1500");
+		textValue.setBounds(10, 137, 96, 19);
+		contentPane.add(textValue);
+		textValue.setColumns(10);
+		
+		lblNewLabel_2 = new JLabel("Digite o Mês:");
+		lblNewLabel_2.setBounds(116, 62, 78, 13);
+		contentPane.add(lblNewLabel_2);
+		
+		lblNewLabel_3 = new JLabel("Digite o Ano:");
+		lblNewLabel_3.setBounds(116, 88, 78, 13);
+		contentPane.add(lblNewLabel_3);
+		
+		spinnerMes = new JSpinner();
+		spinnerMes.setModel(new SpinnerNumberModel(0, 0, 12, 1));
+		spinnerMes.setBounds(204, 59, 68, 20);
+		contentPane.add(spinnerMes);
+		
+		spinnerAno = new JSpinner();
+		spinnerAno.setModel(new SpinnerNumberModel(2023, 2000, 2050, 1));
+		spinnerAno.setBounds(204, 85, 68, 20);
+		contentPane.add(spinnerAno);
+		
+		btnSend = new JButton("Finalizar");
+		btnSend.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					btnDoneActionPressed();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnSend.setBounds(121, 188, 85, 21);
+		contentPane.add(btnSend);
+		
+		comboBox = new JComboBox();
+		comboBox.setBounds(10, 33, 133, 21);
+		contentPane.add(comboBox);
+		
+		lblNewLabel = new JLabel("Escolha o Investimento a ser Editado:");
+		lblNewLabel.setBounds(10, 10, 184, 13);
+		contentPane.add(lblNewLabel);
 	}
 
+	protected void btnDoneActionPressed() throws SQLException, IOException {
+		FundoDespesasOcasionais invest = (FundoDespesasOcasionais) comboBox.getSelectedItem();
+		
+		invest.setAno((int) spinnerAno.getValue());
+		invest.setMes((int) spinnerMes.getValue());
+		invest.setNome(textName.getText());
+		invest.setValor(Float.parseFloat(textValue.getText()));
+		
+		this.fundoService.editarFundoDespesasOcasionais(invest);
+		setVisible(false);
+	}
 }
--- src/gui/ExclusaoFundoWindow.java
+++ src/gui/ExclusaoFundoWindow.java
@@ -1,14 +1,37 @@
 package gui;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+
+import entities.CategoriaDespesa;
+import entities.CategoriaRendimento;
+import entities.*;
+import service.CategoriaDespesaService;
+import service.CategoriaRendimentoService;
+import service.DespesasService;
+import service.FundoDespesasOcasionaisService;
+import service.InvestimentoService;
 
 public class ExclusaoFundoWindow extends JFrame {
 
 	private JPanel contentPane;
+	private JButton btnDone;
+	private JComboBox comboChoice;
+	private JLabel lblName;
+	private FundoDespesasOcasionaisService fundoService;
 
 	/**
 	 * Launch the application.
@@ -30,12 +53,77 @@ public class ExclusaoFundoWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public ExclusaoFundoWindow() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setResizable(false);
+		this.initComponents();
+		this.fundoService = new FundoDespesasOcasionaisService();
+		
+		try {
+			buscarFundos();
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null,"SQLException", "Error", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null,"IOException", "Error", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
+	}
+	
+	private void buscarFundos() throws SQLException, IOException {
+		List<FundoDespesasOcasionais> despesas = this.fundoService.buscarFundoDespesasOcasionais();
+		
+		for (FundoDespesasOcasionais despesa : despesas) {
+			this.comboChoice.addItem(despesa);
+		}
+		
+	}
+
+	private void initComponents() {
+		setTitle("Exclusão de Fundos");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 335, 226);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
+		
+		lblName = new JLabel("Escolha o Fundo a ser Excluido:");
+		lblName.setBounds(10, 10, 281, 13);
+		contentPane.add(lblName);
+		
+		comboChoice = new JComboBox();
+		comboChoice.setBounds(10, 33, 281, 21);
+		contentPane.add(comboChoice);
+		
+		btnDone = new JButton("Finalizar");
+		btnDone.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					btnDonePressed();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
 
+			
+		});
+		btnDone.setBounds(100, 144, 85, 21);
+		contentPane.add(btnDone);
+	}
+	
+	private void btnDonePressed() throws SQLException, IOException {
+		Investimento invest = (Investimento) this.comboChoice.getSelectedItem();
+		
+		int choice = JOptionPane.showConfirmDialog(null, "Deseja realmente excluir este investimento?", "Confirmação", JOptionPane.YES_NO_OPTION);
+		if (choice == 0) {
+			this.fundoService.excluirFundoDespesasOcasionais(invest.getCodigo());
+		}
+		this.setVisible(false);
+	}
+	
+	
 }
